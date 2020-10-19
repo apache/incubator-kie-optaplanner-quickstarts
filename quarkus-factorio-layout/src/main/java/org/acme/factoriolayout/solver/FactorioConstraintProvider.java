52eb030804f06bab99ec18fc1fe34b7d5ea21374
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2020-10-19 14:26:54 +0200

    Factorio quickstart: more constraints

--- quarkus-factorio-layout/src/main/java/org/acme/factoriolayout/solver/FactorioConstraintProvider.java
+++ quarkus-factorio-layout/src/main/java/org/acme/factoriolayout/solver/FactorioConstraintProvider.java
@@ -16,6 +16,7 @@
 
 package org.acme.factoriolayout.solver;
 
+import org.acme.factoriolayout.bootstrap.FactorioDataGenerator;
 import org.acme.factoriolayout.domain.Assembly;
 import org.optaplanner.core.api.score.buildin.hardsoftlong.HardSoftLongScore;
 import org.optaplanner.core.api.score.stream.Constraint;
@@ -23,6 +24,8 @@ import org.optaplanner.core.api.score.stream.ConstraintFactory;
 import org.optaplanner.core.api.score.stream.ConstraintProvider;
 import org.optaplanner.core.api.score.stream.Joiners;
 
+import static org.optaplanner.core.api.score.stream.ConstraintCollectors.max;
+import static org.optaplanner.core.api.score.stream.ConstraintCollectors.min;
 import static org.optaplanner.core.api.score.stream.Joiners.filtering;
 import static org.optaplanner.core.api.score.stream.Joiners.lessThan;
 import static org.optaplanner.core.api.score.stream.Joiners.lessThanOrEqual;
@@ -32,9 +35,12 @@ public class FactorioConstraintProvider implements ConstraintProvider {
     @Override
     public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
         return new Constraint[] {
-            areaConflict(constraintFactory),
-            manhattanDistance(constraintFactory),
-            downstreamY(constraintFactory)
+                areaConflict(constraintFactory),
+                downstreamY(constraintFactory),
+                manhattanDistance(constraintFactory),
+                groupRecipesByY(constraintFactory),
+//                frontLoadAssemblies(constraintFactory),
+                centerAssemblies(constraintFactory),
         };
     }
 
@@ -43,18 +49,6 @@ public class FactorioConstraintProvider implements ConstraintProvider {
                 .fromUniquePair(Assembly.class,
                         Joiners.equal(Assembly::getArea))
                 .penalize("Area conflict", HardSoftLongScore.ONE_HARD);
-    }
-
-    Constraint manhattanDistance(ConstraintFactory constraintFactory) {
-        return constraintFactory
-                // Downstream assembly
-                .from(Assembly.class)
-                // Upstream assembly
-                .join(Assembly.class,
-                        filtering((down, up)-> down.getInputAssemblyList().contains(up)))
-                .penalizeLong("Manhattan distance", HardSoftLongScore.ONE_SOFT,
-                        (down, up) -> Math.abs(down.getArea().getX() - up.getArea().getX())
-                                    + Math.abs(down.getArea().getY() - up.getArea().getY()));
     }
 
     Constraint downstreamY(ConstraintFactory constraintFactory) {
@@ -69,5 +63,50 @@ public class FactorioConstraintProvider implements ConstraintProvider {
                         // At least 1 when upY == downY
                         (down, up) -> up.getArea().getY() - down.getArea().getY() + 1);
     }
+
+    Constraint manhattanDistance(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                // Downstream assembly
+                .from(Assembly.class)
+                // Upstream assembly
+                .join(Assembly.class,
+                        filtering((down, up)-> down.getInputAssemblyList().contains(up)))
+                .penalizeLong("Manhattan distance", HardSoftLongScore.ofSoft(1000L),
+                        (down, up) -> Math.abs(down.getArea().getX() - up.getArea().getX())
+                                + Math.abs(down.getArea().getY() - up.getArea().getY()));
+    }
+
+    Constraint groupRecipesByY(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                .from(Assembly.class)
+                .groupBy(Assembly::getRecipe,
+                        (Assembly assembly) -> 1,
+                        min((Assembly assembly) -> assembly.getArea().getY()),
+                        max((Assembly assembly) -> assembly.getArea().getY()))
+                .filter((recipe, workaroundConstant, minY, maxY) -> !minY.equals(maxY))
+                .penalizeLong("Group recipes by Y", HardSoftLongScore.ofSoft(10L),
+                        (recipe, workaroundConstant, minY, maxY) -> (long) (maxY - minY));
+    }
+
+    Constraint frontLoadAssemblies(ConstraintFactory constraintFactory) {
+        return constraintFactory.from(Assembly.class)
+                .penalizeLong("Front load assemblies", HardSoftLongScore.ofSoft(1L),
+                        assembly -> assembly.getArea().getY());
+    }
+
+    Constraint centerAssemblies(ConstraintFactory constraintFactory) {
+        return constraintFactory.from(Assembly.class)
+                .penalizeLong("Center assemblies", HardSoftLongScore.ofSoft(1L),
+                        assembly -> Math.abs(FactorioDataGenerator.DEFAULT_AREA_WIDTH - assembly.getArea().getX()));
+    }
+
+    // TODO make FactorioDataGenerator.DEFAULT_AREA_WIDTH private and use this instead
+//    Constraint centerAssembly(ConstraintFactory constraintFactory) {
+//        return constraintFactory
+//                .from(FactorioLayout.class) // the solution class
+//                .join(Assembly.class)
+//                .penalizeLong("Center assembly", HardSoftLongScore.ofSoft(1L),
+//                        (factorioLayout, assembly) -> Math.abs((factorioLayout.getAreaWidth() / 2) - assembly.getArea().getX()));
+//    }
 
 }
