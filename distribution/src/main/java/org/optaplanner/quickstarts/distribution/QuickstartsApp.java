332e6e05dab8c06693c598cd0022b72811862cab
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2020-11-02 12:40:38 +0100

    Aggregation POC

---
+++ distribution/src/main/java/org/optaplanner/quickstarts/distribution/QuickstartsApp.java
@@ -0,0 +1,12 @@
+package org.optaplanner.quickstarts.distribution;
+
+import javax.swing.JOptionPane;
+
+public class QuickstartsApp {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "hello world");
+        System.out.println("Hello world");
+    }
+
+}
