cbb0ca8cb9cf5281ed388deffc8eb052c283a4e1
Jiří Locker <jiri.locker@gmail.com>
2020-11-27 16:07:18 +0100

    Use Maven Wrapper in the run script

--- runQuickstartsWithMaven.sh
+++ runQuickstartsWithMaven.sh
@@ -3,6 +3,5 @@
 # Change directory to the directory of the script
 cd "$(dirname $0)" || exit
 
-mvn verify -DskipTests
-cd build/quickstarts-showcase || exit
-mvn quarkus:dev -Dstartup-open-browser=true
+./mvnw verify -DskipTests &&
+  ./mvnw -f build/quickstarts-showcase quarkus:dev -Dstartup-open-browser=true
