b92c5226ff92ae13e441ef40267fff6877137885
Jiří Locker <jiri.locker@gmail.com>
2020-11-27 16:07:18 +0100

    Use Maven Wrapper in the run script

--- runQuickstartsWithMaven.bat
+++ runQuickstartsWithMaven.bat
@@ -1,6 +1,5 @@
 @ECHO OFF
 setLocal enableExtensions enableDelayedExpansion
 
-mvn verify -DskipTests
-cd build\quickstarts-showcase
-mvn quarkus:dev -Dstartup-open-browser=true
+mvnw verify -DskipTests && ^
+mvnw -f build\quickstarts-showcase quarkus:dev -Dstartup-open-browser=true
