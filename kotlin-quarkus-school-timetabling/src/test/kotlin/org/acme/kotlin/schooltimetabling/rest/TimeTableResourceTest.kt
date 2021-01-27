9c958bcd4ac31329ba9fc5d76491711dc8255e67
Radovan Synek <rsynek@redhat.com>
2021-01-27 09:38:48 +0100

    PLANNER-2254 Fix flaky TimeTableControllerTest

--- kotlin-quarkus-school-timetabling/src/test/kotlin/org/acme/kotlin/schooltimetabling/rest/TimeTableResourceTest.kt
+++ kotlin-quarkus-school-timetabling/src/test/kotlin/org/acme/kotlin/schooltimetabling/rest/TimeTableResourceTest.kt
@@ -37,12 +37,12 @@ class TimeTableResourceTest {
     fun solveDemoDataUntilFeasible() {
         timeTableResource.solve()
         var timeTable: TimeTable = timeTableResource.getTimeTable()
-        while (timeTable.solverStatus != SolverStatus.NOT_SOLVING) {
+        do { // Use do-while to give the solver some time and avoid retrieving an early infeasible solution.
             // Quick polling (not a Test Thread Sleep anti-pattern)
             // Test is still fast on fast machines and doesn't randomly fail on slow machines.
             Thread.sleep(20L)
             timeTable = timeTableResource.getTimeTable()
-        }
+        } while (timeTable.solverStatus != SolverStatus.NOT_SOLVING)
         Assertions.assertFalse(timeTable.lessonList.isEmpty())
         for (lesson in timeTable.lessonList) {
             Assertions.assertNotNull(lesson.timeslot)
