3b4e4ec246e8e4b2024e37681da7750503f69cf8
Jiří Locker <jiri.locker@gmail.com>
2021-09-06 15:12:20 +0200

    Remove segmented

--- use-cases/vehicle-routing/src/main/java/org/acme/vehiclerouting/domain/location/DistanceType.java
+++ use-cases/vehicle-routing/src/main/java/org/acme/vehiclerouting/domain/location/DistanceType.java
@@ -26,8 +26,4 @@ public enum DistanceType {
      * Requires that all {@link Location} instances are of type {@link RoadLocation}.
      */
     ROAD_DISTANCE,
-    /**
-     * Requires that all {@link Location} instances are of type {@link RoadSegmentLocation} or {@link HubSegmentLocation}.
-     */
-    SEGMENTED_ROAD_DISTANCE;
 }
