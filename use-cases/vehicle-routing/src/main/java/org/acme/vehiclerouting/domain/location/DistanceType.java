4cd4139dc2910247cfcd7ba0a13dbdd1807627d0
Jiří Locker <jiri.locker@gmail.com>
2021-09-06 15:14:20 +0200

    Remove RoadLocation

--- use-cases/vehicle-routing/src/main/java/org/acme/vehiclerouting/domain/location/DistanceType.java
+++ use-cases/vehicle-routing/src/main/java/org/acme/vehiclerouting/domain/location/DistanceType.java
@@ -22,8 +22,4 @@ public enum DistanceType {
      * Requires that all {@link Location} instances are of type {@link AirLocation}.
      */
     AIR_DISTANCE,
-    /**
-     * Requires that all {@link Location} instances are of type {@link RoadLocation}.
-     */
-    ROAD_DISTANCE,
 }
