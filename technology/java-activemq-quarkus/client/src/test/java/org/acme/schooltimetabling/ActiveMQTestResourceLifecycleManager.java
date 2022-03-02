a83c9f43b3a5ef8f5d02f73bcc99a381f48f1c73
Lukáš Petrovický <lpetrovi@redhat.com>
2022-03-02 11:53:36 +0100

    Bump Quarkus and other dependencies
    Signed-off-by: Lukáš Petrovický <lpetrovi@redhat.com>

--- technology/java-activemq-quarkus/client/src/test/java/org/acme/schooltimetabling/ActiveMQTestResourceLifecycleManager.java
+++ technology/java-activemq-quarkus/client/src/test/java/org/acme/schooltimetabling/ActiveMQTestResourceLifecycleManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2022 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
-import io.smallrye.reactive.messaging.connectors.InMemoryConnector;
+import io.smallrye.reactive.messaging.providers.connectors.InMemoryConnector;
 
 public class ActiveMQTestResourceLifecycleManager implements QuarkusTestResourceLifecycleManager {
 
