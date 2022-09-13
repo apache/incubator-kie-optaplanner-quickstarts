f34622f29b9274391b3b2f94cc19379fa50c920d
Lukáš Petrovický <lpetrovi@redhat.com>
2022-09-13 08:38:52 +0200

    Attempt to finish the PR

--- technology/java-spring-boot/src/test/java/org/acme/schooltimetabling/rest/TimeTableControllerTest.java
+++ technology/java-spring-boot/src/test/java/org/acme/schooltimetabling/rest/TimeTableControllerTest.java
@@ -1,44 +1,50 @@
 package org.acme.schooltimetabling.rest;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
 
-import org.acme.schooltimetabling.domain.Lesson;
-import org.acme.schooltimetabling.domain.TimeTable;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.optaplanner.core.api.solver.SolverStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.time.Duration;
 
 @SpringBootTest(properties = {
         // Effectively disable spent-time termination in favor of the best-score-limit
         "optaplanner.solver.termination.spent-limit=1h",
-        "optaplanner.solver.termination.best-score-limit=0hard/*soft"})
+        "optaplanner.solver.termination.best-score-limit=0hard/*soft" },
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class TimeTableControllerTest {
-
-    @Autowired
-    private TimeTableController timeTableController;
 
     @Test
     @Timeout(600_000)
-    public void solveDemoDataUntilFeasible() throws InterruptedException {
-        timeTableController.solve();
-        TimeTable timeTable;
-        do { // Use do-while to give the solver some time and avoid retrieving an early infeasible solution.
-            // Quick polling (not a Test Thread Sleep anti-pattern)
-            // Test is still fast on fast machines and doesn't randomly fail on slow machines.
-            Thread.sleep(20L);
-            timeTable = timeTableController.getTimeTable();
-        } while (timeTable.getSolverStatus() != SolverStatus.NOT_SOLVING || !timeTable.getScore().isFeasible());
+    public void solveDemoDataUntilFeasible() {
+        given()
+                .contentType(ContentType.JSON)
+                .when().post("/timeTable/solve")
+                .then()
+                .statusCode(200);
 
-        assertFalse(timeTable.getLessonList().isEmpty());
-        for (Lesson lesson : timeTable.getLessonList()) {
-            assertNotNull(lesson.getTimeslot());
-            assertNotNull(lesson.getRoom());
-        }
-        assertTrue(timeTable.getScore().isFeasible());
+        await()
+                .atMost(Duration.ofMinutes(1))
+                .pollDelay(Duration.ofSeconds(5))
+                .pollInterval(Duration.ofSeconds(5))
+                .until(() -> SolverStatus.NOT_SOLVING.name().equals(get("/timeTable").body().path("solverStatus")));
+
+        get("/timeTable").then().assertThat()
+                .body("solverStatus", equalTo(SolverStatus.NOT_SOLVING.name()))
+                .body("timeslotList", is(not(empty())))
+                .body("roomList", is(not(empty())))
+                .body("lessonList", is(not(empty())))
+                .body("lessonList.timeslot", not(nullValue()))
+                .body("lessonList.room", not(nullValue()));
     }
 
 }
