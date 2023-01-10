224f6787d53af3a0202fd768611904f3a708e3c2
Christopher Chianelli <chris.chianelli@gmail.com>
2023-01-10 14:49:37 -0500

    Upgrade to Spring Boot 3.x
    - PagingAndSortingRepository no longer extends CrudRepository, so
      now our Repositories need to extend both PagingAndSortingRepository
      and CrudRepository to get the same functionality/methods.
    
    - Migrated from javax to jakarta
    
    - Spring Boot 3.x only support Java 17, so upgraded the release version

--- technology/java-spring-boot/src/main/java/org/acme/schooltimetabling/persistence/TimeslotRepository.java
+++ technology/java-spring-boot/src/main/java/org/acme/schooltimetabling/persistence/TimeslotRepository.java
@@ -2,11 +2,12 @@ package org.acme.schooltimetabling.persistence;
 
 import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import org.acme.schooltimetabling.domain.Timeslot;
 
-public interface TimeslotRepository extends PagingAndSortingRepository<Timeslot, Long> {
+public interface TimeslotRepository extends CrudRepository<Timeslot, Long>, PagingAndSortingRepository<Timeslot, Long> {
 
     @Override
     List<Timeslot> findAll();
