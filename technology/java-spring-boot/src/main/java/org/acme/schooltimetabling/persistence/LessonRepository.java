f19b6d3815e8ef0ed2548d1b94c439865901d198
Radovan Synek <rsynek@redhat.com>
2023-04-13 16:31:40 +0200

    Migrate java-spring-boot

--- technology/java-spring-boot/src/main/java/org/acme/schooltimetabling/persistence/LessonRepository.java
+++ technology/java-spring-boot/src/main/java/org/acme/schooltimetabling/persistence/LessonRepository.java
@@ -2,11 +2,12 @@ package org.acme.schooltimetabling.persistence;
 
 import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import org.acme.schooltimetabling.domain.Lesson;
 
-public interface LessonRepository extends PagingAndSortingRepository<Lesson, Long> {
+public interface LessonRepository extends CrudRepository<Lesson, Long>, PagingAndSortingRepository<Lesson, Long> {
 
     @Override
     List<Lesson> findAll();
--- technology/java-spring-boot/src/main/java/org/acme/schooltimetabling/persistence/RoomRepository.java
+++ technology/java-spring-boot/src/main/java/org/acme/schooltimetabling/persistence/RoomRepository.java
@@ -2,11 +2,12 @@ package org.acme.schooltimetabling.persistence;
 
 import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import org.acme.schooltimetabling.domain.Room;
 
-public interface RoomRepository extends PagingAndSortingRepository<Room, Long> {
+public interface RoomRepository extends CrudRepository<Room, Long>, PagingAndSortingRepository<Room, Long> {
 
     @Override
     List<Room> findAll();
